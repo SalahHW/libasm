d23f7ac8460c9ad3ce31f00724fae206edc600d3
SalahHW <salah.bouheniche@gmail.com>
2026-02-21 04:59:51 +0100

    refactor: add register aliases for readability

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,17 +1,22 @@
+; size_t ft_strlen(const char *s);
+%define length          rax
+%define current_char    rsi 
+%define str             rdi
+
 section .data
 
 section .text
 global ft_strlen
 
 ft_strlen:
-mov rax, 0
+xor length, length 
 
 
 start_loop:
-movzx rsi, byte [rdi + rax]
-cmp rsi, 0
+movzx current_char, byte [str + length]
+cmp current_char, 0
 je loop_exit
-inc rax
+inc length
 jmp start_loop
 
 loop_exit:
